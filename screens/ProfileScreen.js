19c73e3b744d17e69ba145c0d348ecf517d6a680
Theo Breaux <theobreaux@gmail.com>
2025-04-30 13:45:22 -0700

    *

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, ScrollView, TextInput, TouchableOpacity, Image } from 'react-native'
+import { View, Text, ScrollView, TextInput, TouchableOpacity, KeyboardAvoidingView, Platform } from 'react-native'
 import LogoScreenWrapper from '../components/LogoScreenWrapper'
 import Styles from '../style'
 import { ASSET_URL, CIRCLE_SIZE, COLORS, SCREEN_HEIGHT, SCREEN_WIDTH } from '../constants/constants'
@@ -8,7 +8,6 @@ import { useSelector } from 'react-redux'
 import ProfileImageMedallion from '../components/ui/ProfileImageMedallion'
 import Ionicons from '@expo/vector-icons/Ionicons'
 import { scaleFont } from '../utils/utils'
-import Images from '../assets/images/images'
 
 const ProfileScreen = ({ navigation }) => {
   const user = useSelector((state) => state.app.user)
@@ -20,88 +19,105 @@ const ProfileScreen = ({ navigation }) => {
 
   return (
     <LogoScreenWrapper backgroundColor={COLORS.logoScreenBackground}>
-      <ProfileImageMedallion
-        imageUrl={ASSET_URL + user.imgUrl}
-        height={CIRCLE_SIZE * 0.5}
-        width={CIRCLE_SIZE * 0.5}
-        borderRadius={(CIRCLE_SIZE * 0.5) / 2}
-      />
-
-      <ScrollView contentContainerStyle={Styles.profileScreen.scrollViewContainer}>
-        <Text style={{ fontSize: 22, fontFamily: 'Poppins-ExtraBold', marginTop: 10 }}>
-          Member since:
-          <Text style={{ fontSize: 20, fontFamily: 'Poppins-Medium' }}> {formattedDate}</Text>
-        </Text>
-        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-          <Text style={{ fontSize: 30, fontFamily: 'Poppins-ExtraBold' }}>{user.firstName + ' ' + user.lastName}</Text>
-          <TouchableOpacity>
-            <Ionicons
-              name={'heart-outline'}
-              size={SCREEN_WIDTH < 400 ? 30 : 40}
-              color={COLORS.goDutchBlue}
-            />
-          </TouchableOpacity>
-        </View>
-
-        <View
-          style={{
-            width: SCREEN_WIDTH * 0.9,
-            height: 'auto',
-            borderWidth: 2,
-            borderRadius: 15,
-            padding: 10,
-            elevation: 5,
-            backgroundColor: 'white',
-            borderRadius: 10,
-            shadowColor: 'black',
-            shadowOffset: { width: 0, height: 2 },
-            shadowOpacity: 0.2,
-            shadowRadius: 4,
-          }}
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        style={{ flex: 1 }}
+      >
+        <ScrollView
+          contentContainerStyle={Styles.profileScreen.scrollViewContainer}
+          showsVerticalScrollIndicator={false}
         >
+          <ProfileImageMedallion
+            imageUrl={ASSET_URL + user.imgUrl}
+            height={CIRCLE_SIZE * 0.5}
+            width={CIRCLE_SIZE * 0.5}
+            borderRadius={(CIRCLE_SIZE * 0.5) / 2}
+          />
+
+          <Text style={{ fontSize: 22, fontFamily: 'Poppins-ExtraBold', marginTop: 10 }}>
+            Member since:
+            <Text style={{ fontSize: 20, fontFamily: 'Poppins-Medium' }}> {formattedDate}</Text>
+          </Text>
           <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-            <Text style={{ fontSize: scaleFont(30), fontFamily: 'Poppins-ExtraBold' }}>About</Text>
-            <Text style={{ fontSize: scaleFont(20), fontFamily: 'Poppins-ExtraBold', color: COLORS.goDutchRed }}>@{user.username}</Text>
+            <Text style={{ fontSize: 30, fontFamily: 'Poppins-ExtraBold' }}>{user.firstName + ' ' + user.lastName}</Text>
+            <TouchableOpacity>
+              <Ionicons
+                name={'heart-outline'}
+                size={SCREEN_WIDTH < 400 ? 30 : 40}
+                color={COLORS.goDutchBlue}
+              />
+            </TouchableOpacity>
           </View>
 
-          <Text style={{ fontSize: scaleFont(18), fontFamily: 'Poppins-Regular' }}>{user.bio}</Text>
-
-          <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-            <View style={{ flexDirection: 'column' }}>
-              <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                <Text style={{ fontSize: scaleFont(20), fontFamily: 'Poppins-ExtraBold' }}>Location: </Text>
-                <Text style={{ fontSize: scaleFont(18), fontFamily: 'Poppins-Regular' }}>{user.location}</Text>
-              </View>
-
-              <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                <Text style={{ fontSize: scaleFont(20), fontFamily: 'Poppins-ExtraBold' }}>Birthday: </Text>
-                <Text style={{ fontSize: scaleFont(18), fontFamily: 'Poppins-Regular' }}>{user.birthday}</Text>
-              </View>
-              <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                <Text style={{ fontSize: scaleFont(20), fontFamily: 'Poppins-ExtraBold' }}>Favorite Cuisine: </Text>
-                <Text style={{ fontSize: scaleFont(18), fontFamily: 'Poppins-Regular' }}>{user.favoriteCuisine}</Text>
-              </View>
+          <View
+            style={{
+              width: SCREEN_WIDTH * 0.9,
+              marginBottom: 50,
+              height: 'auto',
+              borderWidth: 2,
+              borderRadius: 15,
+              padding: 10,
+              elevation: 5,
+              backgroundColor: 'white',
+              borderRadius: 10,
+              shadowColor: 'black',
+              shadowOffset: { width: 0, height: 2 },
+              shadowOpacity: 0.2,
+              shadowRadius: 4,
+            }}
+          >
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+              <Text style={{ fontSize: scaleFont(30), fontFamily: 'Poppins-ExtraBold' }}>About</Text>
+              <Text style={{ fontSize: scaleFont(20), fontFamily: 'Poppins-ExtraBold', color: COLORS.goDutchRed }}>@{user.username}</Text>
             </View>
 
-            <PrimaryButton
-              onPress={() => navigation.goBack()}
-              outerWidth={SCREEN_WIDTH * 0.28}
-              innerWidth={SCREEN_WIDTH * 0.26}
-            >
-              Return
-            </PrimaryButton>
+            <Text style={{ fontSize: scaleFont(18), fontFamily: 'Poppins-Regular' }}>{user.bio}</Text>
+
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+              <View style={{ flexDirection: 'column' }}>
+                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                  <Text style={{ fontSize: scaleFont(20), fontFamily: 'Poppins-ExtraBold' }}>Location: </Text>
+                  <Text style={{ fontSize: scaleFont(18), fontFamily: 'Poppins-Regular' }}>{user.location}</Text>
+                </View>
+
+                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                  <Text style={{ fontSize: scaleFont(20), fontFamily: 'Poppins-ExtraBold' }}>Birthday: </Text>
+                  <Text style={{ fontSize: scaleFont(18), fontFamily: 'Poppins-Regular' }}>{user.birthday}</Text>
+                </View>
+                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                  <Text style={{ fontSize: scaleFont(20), fontFamily: 'Poppins-ExtraBold' }}>Favorite Cuisine: </Text>
+                  <Text style={{ fontSize: scaleFont(18), fontFamily: 'Poppins-Regular' }}>{user.favoriteCuisine}</Text>
+                </View>
+              </View>
+
+              <PrimaryButton
+                onPress={() => navigation.goBack()}
+                outerWidth={SCREEN_WIDTH * 0.28}
+                innerWidth={SCREEN_WIDTH * 0.26}
+              >
+                Return
+              </PrimaryButton>
+            </View>
+
+            <TextInput
+              style={{
+                borderWidth: 1,
+                borderColor: '#555151',
+                borderRadius: 5,
+                padding: 10,
+                fontSize: 14,
+                fontFamily: 'Poppins-Regular',
+                height: SCREEN_HEIGHT * 0.095,
+              }}
+              multiline={true}
+              numberOfLines={3}
+              // onChangeText={handleChangeNotes}
+              // value={notes}
+              placeholder="Enter your notes..."
+            />
           </View>
-        </View>
-      </ScrollView>
-      <Image
-        source={Images.go_dutch_background}
-        style={{
-          resizeMode: 'center',
-          position: 'absolute',
-          top: 130,
-          zIndex: -1,
-        }}
-      />
+        </ScrollView>
+      </KeyboardAvoidingView>
     </LogoScreenWrapper>
   )
 }
